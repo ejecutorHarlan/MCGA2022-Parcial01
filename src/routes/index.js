b59c372b1d287b45222dc0205b432f86fe0a99a0
ejecutorHarlan <54726387+ejecutorHarlan@users.noreply.github.com>
2022-09-27 21:02:41 -0300

    Push del versel.json

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,13 @@
-const express = require("express");
+const express = require('express');
 
 const router = express.Router();
-const productsRoutes = require("./products");
-const provRoutes = require("./proveedores");
+const productsRoutes = require('./products');
+const provRoutes = require('./proveedores');
 
-router.use("/api/products", productsRoutes);
-router.use("/api/prov", provRoutes);
-
-
+router.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+router.use('/api/products', productsRoutes);
+router.use('/api/prov', provRoutes);
 
 module.exports = router;
